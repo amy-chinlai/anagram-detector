04d3e5cb93e1530988ac9bb4d39a169d826da20b
amy-chinlai <amy@actionnetwork.org>
2021-07-06 12:23:04 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        result = []
+        # array.each do |word|
+        #     scramble = @word.split("").sort.join
+        #     if scramble == word
+        #         result << scramble
+        #     end
+        scramble = @word.split("").sort.join
+        array.each do |matchee|
+            if matchee == scramble
+                result << matchee
+            end
+        end
+        result
+    end
+end
