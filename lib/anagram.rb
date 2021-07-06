8f2c56bf9ee1875d181950dfe4410a2324ea1f79
amy-chinlai <amy@actionnetwork.org>
2021-07-06 12:34:59 -0400

    commit tree 30433e0b3432a680f5813f740061ed88aa37d34f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,10 +9,10 @@ class Anagram
 
     def match(array)
         result = []
+        scramble = @word.split("").sort.join
         array.each do |word|
-            scramble = @word.split("").sort.join
-            if scramble == word
-                result << scramble
+            if scramble == word.split("").sort.join
+                result << word
             end
         end
         result
